2f185baa92bb4f521c1ac5c62364555478bc91dd
asceticmonker <cy_ascetic@163.com>
2016-04-12 00:56:18 +0800

    edit

--- blog/app.js
+++ blog/app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -18,10 +19,11 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+routes(app);
 app.use('/', routes);
 app.use('/users', users);
 
